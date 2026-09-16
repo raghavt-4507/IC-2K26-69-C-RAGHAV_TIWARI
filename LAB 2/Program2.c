8eba21f7d3313719c8d8e9a814dd99fde69cc277
raghavt-4507 <raghavtiwari8390@gmail.com>
2026-09-16 18:50:49 +0530

    Update Program2.c

--- LAB 2/Program2.c
+++ LAB 2/Program2.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main()
+{
+    int a = 20;
+    int b = 10;
+    int difference;
+
+    difference = a - b;
+
+    printf("Difference = %d", difference);
+
+    return 0;
+}
